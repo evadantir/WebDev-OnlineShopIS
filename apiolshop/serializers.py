a315ac955314c04c9012134a6d88a5fc608f5214
Eva Danti Rahmanita <evadantir@yahoo.com>
2017-01-13 09:38:24 +0700

    update barang serializers

--- apiolshop/serializers.py
+++ apiolshop/serializers.py
@@ -2,30 +2,25 @@ from .models import *
 from rest_framework import serializers
 
 class BarangSerializer(serializers.ModelSerializer):
-    kode_tipe = serializers.StringRelatedField(read_only=True)
-
+    kode_tipe = serializers.PrimaryKeyRelatedField(queryset=TipeBarang.objects.all())
     class Meta:
         model = Barang
         fields = '__all__'
-        #depth = 1
 
 class BarangPesananSerializer(serializers.ModelSerializer):
     class Meta:
         model = BarangPesanan
         fields = '__all__'
-        #depth = 1
 
 class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = '__all__'
-        #depth = 1
 
 class TransaksiSerializer(serializers.ModelSerializer):
     class Meta:
         model = Transaksi
         fields = '__all__'
-        #depth = 1
 
 class TipeBarangSerializer(serializers.ModelSerializer):
     class Meta:
@@ -51,4 +46,3 @@ class DaftarAlamatSerializer(serializers.ModelSerializer):
     class Meta:
         model = DaftarAlamat
         fields = '__all__'
-        #depth = 1
